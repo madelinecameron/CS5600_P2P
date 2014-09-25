a50021fc725bea3c42c5daf1903b01de9b94d43b
Madeline <madeline@madelinecameron.net>
2014-09-25 09:06:00 -0500

    Started main.c
    Beginning code for creating sockets

---
+++ server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H_INCLUDED
+#define SERVER_H_INCLUDED
+
+/*
+ * server.h
+ *
+ *  Created on: Sep 25, 2014
+ *      Author: Madeline
+ */
+
+#define PORT 5600
+
+#include <sys/socket.h>
+
+<connection> peerlist
+
+command recieveCommand()
+void sendTracker()
+tracker createTracker()
+bool updateTracker()
+void runServer()
+
+
+#endif // SERVER_H_INCLUDED
