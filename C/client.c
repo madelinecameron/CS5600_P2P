081bd245db8e4b2b9484fde1e4bdb2f3b51e8eac
mdlkwd <mdlkwd@mst.edu>
2014-10-10 05:42:42 -0500

    C Code - added some comments
    Added some ideas in the comments.

--- C/client.c
+++ C/client.c
@@ -1,3 +1,4 @@
+/* gcc client.c -o client -lnsl -pthread */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,24 +8,26 @@
 #include <pthread.h>
 #include <signal.h>
 
-/* gcc client.c -o client -lnsl -pthread */
-
 #define SERVER_PORT 63122
 
+//Specifies address: Where we are connecting our socket.
+struct sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) }; 
+struct hostent *hp; 
+
+//socket used to connect to server, and read data from
+int sd;
+//File pointer, Points to the file we want to read/write to.
+FILE *file;
+//Buffer used to send/receive. Data is loaded into buffer (see read() and fwrite()).
+char buf[512]; 
 
 int main()
 {
-	int sd; //socket used to connect to server
-	struct sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) }; 
-	char buf[512]; 
-	struct hostent *hp; 
-
-	FILE *file;
-	
+  //Example host name: “rc01xcs213.managed.mst.edu”	
 	printf("Please Enter the Server You Would Like to Connect To\n");
 	scanf("%s", buf); //read the server address from the keyboard
 	
-	/* get the host */
+	/* Get host via the server address entered by user into buf*/
 	if((hp = gethostbyname(buf)) == NULL)
 	{
 		printf("Error: Unkown Host");
@@ -33,33 +36,41 @@ int main()
 	bcopy( hp->h_addr_list[0], (char*)&server_addr.sin_addr, hp->h_length ); 
 	
 	/* create a socket */
-	if( ( sd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )  //internet stream socket, TCP
+	/* internet stream socket, TCP */
+	if( ( sd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )  
     { 
 		perror( "Error: socket failed" ); 
 		exit( 1 ); 
 	} 
 		
 	/*connect to the socket */
+	/* now when we need to communicate to the server, we do it over "sd" */
 	if (connect( sd, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
 	{
 		perror( "Error: Connection Issue" ); 
 		exit(1);
 	}
 
+	//Creates a file to be written to. "Recieve.txt" should be replaced with the actual name of the file
 	if((file = fopen("receive.txt", "wb")) == NULL) //w for write, b for binary
 	{
 		printf("Error writing new file\n");
 		exit(1);
 	}
 
-	//clear the buffer
+	//Clear the buffer
+	/* This might not be necessary. Since the test file I was sending with the server was < 512 bytes, I used strlen(buf)
+	   to only write the contents of the "send.txt" file. So once it read the first 0, it stopped writing.
+		 I'm not sure how strlen() will handle other files though.*/
 	memset(buf, 0, sizeof(buf));
 
+	/*This should permit continuous reading. As long as someone is sending, the client will continue reading*/
 	while(read(sd, buf, sizeof(buf)) > 0)
 	{
 		fwrite(buf, sizeof(char), strlen(buf), file);
 	}
 
 	close(sd); //close the socket
+	fclose(file); //close the file
 	return (0);
 }
--- C/server.c
+++ C/server.c
@@ -1,3 +1,4 @@
+/* gcc server.c -o server -lnsl -pthread */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,17 +10,14 @@
 #include <string.h>
 #include <time.h>
 
-/* gcc server.c -o server -lnsl -pthread */
-
 #define SERVER_PORT 63122
 #define MAX_CLIENT 10
 #define BUFFER_SIZE 512
 
-struct sockaddr_in server_addr;
-struct sockaddr_in client_addr;
-
-//socket, accept, and read
-int sd, ns, k;
+//socket for hosting the server (sd), socket for the client (ns)
+//We should have a variable for each peer. Use this variable in the peer thread.
+//Maybe construct a peer struct/class? Maybe an array of them? Contains socket variable, thread, etc.
+int sd, ns;
 
 //length of socketaddr structure
 int length;
@@ -36,7 +34,7 @@ int main()
 	struct sockaddr_in client_addr = { AF_INET };
 	length = sizeof( client_addr );
 	
-	/* create a stream socket */
+	/* Create a stream socket. Server will listen on this socket for peers. */
 	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		perror("Server Error: Socket Failed");
@@ -45,7 +43,7 @@ int main()
 	
 	//variable needed for setsokopt call
 	int setsock = 1;
-	//assists in using address
+	
 	if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &setsock, sizeof(setsock)) == -1)
 	{
 		perror("Server Error: Setsockopt failed");
@@ -61,12 +59,17 @@ int main()
 	
 	// listen for clients
 	printf(">>Server is now listening for up to 1 client\n");
-	if (listen(sd, 1) == -1) //1 = length of queue for pending connections
+	if (listen(sd, 1) == -1) //1 = length of queue for pending connections. We'll have to make this longer.
 	{
 		perror("Server Error: Listen failed");
 		exit(1);
 	}
+	
+	//In order to continously listen for clients, I think we'll need a while(!quit); loop, or something.
+	//For an example, see the "server.c" file in the "Chat Room - Reference" folder.
 
+	//If we decide to use a Peer struct, it's socket variable would replace "ns".
+	//Once a peer connects, create a thread.
 	if ((ns = accept(sd, (struct sockaddr*)&client_addr, &length)) == -1)
 	{
 		perror("Server Error: Accept Failed");
@@ -87,10 +90,12 @@ int main()
 	{
 		printf("Error loading file\n");
 	}
-
-
+	/* Perhaps for fread() and write(), we'll use a loop.
+	   Read a chunk, send a chunk. Repeat until entire file is read*/
 	write(ns, buf, strlen(buf));
-
+	
+	
+	fclose(file); // close the file stream
 	close(sd); //close the socket
 
 	return (0);
