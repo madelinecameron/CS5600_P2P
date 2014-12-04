967449f467dbf61b61eff0534b56f0a7824e1c63
Madeline <madeline@madelinecameron.net>
2014-12-04 00:39:50 -0600

    Added .empty file to set up file structure Modified .gitignore to include setup_subfolder.sh

---
+++ test_clients/setup_subfolders.sh
@@ -0,0 +1,34 @@
+#Setup
+DIR="$(cd "$(dirname "$0")" && pwd)"
+echo $DIR
+
+for i in 1 2 3 4 5
+do
+	if [ ! -d $DIR"/client_"$i ]
+	then
+		echo "Creating client_"$i
+		mkdir $DIR"/client_"$i
+	else
+		echo "Removing files from client_"$i
+		rm -f $DIR"/client_"$z/*
+	fi
+	cd $DIR"/client_"$i
+	wget http://www.photosnewhd.com/media/images/picture-wallpaper.jpg
+	cd ..
+done
+
+for z in 6 7 8 9 10
+do
+	if [ ! -d $DIR"/client_"$z ]
+	then
+		echo "Creating client_"$z
+		mkdir $DIR"/client_"$z
+	else
+		echo "Removing files from client_"$z
+		rm -f $DIR"/client_"$z/*
+	fi
+done
+
+
+
+
