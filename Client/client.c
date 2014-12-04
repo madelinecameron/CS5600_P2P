cb759070a203f3bdb50af71b1068df0e92a4f858
mdlkwd <mdlkwd@mst.edu>
2014-12-03 22:56:39 -0600

    Matt's Backup - In case he doesn't merge correctly.

--- Client/client.c
+++ Client/client.c
@@ -11,18 +11,20 @@
 #include <string.h>
 #include <ifaddrs.h>
 #include <unistd.h>
+#include <sys/fcntl.h>
+#include <errno.h>
 
 //we'd read this in from the config file
 #define SERVER_PORT 63122
-#define SEED_PORT 63123
 #define MAX_CLIENT 10
 #define CHUNK_SIZE 512
+
+#include "compute_md5.h"
 
 //socket used to connect to server
 int server_sock;
 //socket used to act as "server"
 int seed_sock;
-pthread_t main_seed_thread;
 
 struct peer
 {
@@ -38,14 +40,20 @@ struct peer peers[MAX_CLIENT];
 void setUpPeerArray();
 int findPeerArrayOpening();
 
+void *download(void * index);
 void *client_handler(void * index);
 void *seed(void * param);
 
+/*
 int findCommand();
 void findIP();
+*/
 
-//Buffer used to send/receive. Data is loaded into buffer (see read() and fwrite()).
-char buf[CHUNK_SIZE];
+#define SEED 0
+#define DOWNLOAD 1
+int mode;
+int client_i;
+int port;
 char* ip;
 
 int main(int argc, const char* argv[])
@@ -67,136 +75,243 @@ int main(int argc, const char* argv[])
 	findIP(); //Sets 'ip' with char* representation of address
 	*/
 	
-	
-	
-	
-	//IF MODE == SEED
-	/* Spin off a seed thread */
-	/* TAKE THE CONTENTS OF THE SEED FUNCTION, PUT IT HERE
-	/* SPIN OFF A CLIENT_HANDLER THREAD
-	 * CREATE TRACKER
-	/* OLD STUFF
-	if (pthread_create(&main_seed_thread, NULL, &seed, NULL) != 0)
+	if (argc < 4)
 	{
-		printf("Error Creating Seed Thread\n");
-		exit(1);
+		exit(0);
 	}
-	*/
-	
-	
-	//IF MODE == DOWNLOAD
-	int sent;
-	while (1)
+	else
 	{
-		fgets(buf, sizeof(buf), stdin);
+		mode = atoi(argv[1]);
+		port = atoi(argv[2]);
+		client_i = atoi(argv[3]);
+	}
+	
+	setUpPeerArray();
+	
+	if (mode == SEED)
+	{
+		/* We should pass a parameter in when executeing ./client.out to signify which port we'll be using */
+		struct sockaddr_in server_addr = {AF_INET, htons( SERVER_PORT +1 )}; //Here
+		struct sockaddr_in client_addr = {AF_INET};
+		int length = sizeof(client_addr);
 		
-		if (strncmp(buf, "<REQ LIST>", strlen("<REQ LIST>")) == 0)
+		if ((seed_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		{
-			/* create a socket */
-			/* internet stream socket, TCP */
-			if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+			perror("Server Error: Socket Failed");
+			exit(1);
+		}
+		
+		/* Variable needed for setsokopt call */
+		int setsock = 1;
+		if(setsockopt(seed_sock, SOL_SOCKET, SO_REUSEADDR, &setsock, sizeof(setsock)) == -1)
+		{
+			perror("Server Error: Setsockopt failed");
+			exit(1);
+		}
+		
+		fcntl(seed_sock, F_SETFL, O_NONBLOCK);
+		
+		if (bind(seed_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1 )
+		{
+			perror("Server Error: Bind Failed");
+			exit(1);
+		}
+		
+		if (listen(seed_sock, MAX_CLIENT) == -1)
+		{
+			perror("Server Error: Listen failed");
+			exit(1);
+		}
+				
+		clock_t cur, start = clock();
+		int cur_chunk = ((client_i - 1)*20) + 1;
+		int count_to_4 = 0;
+		
+		if (client_i == 1)
+		{
+			cur_chunk--;
+		}
+		while (1)
+		{
+			int peer_array_index;
+			
+			/* The server searches for an opening in the client array. If the array is full, -1 is returned */
+			if ((peer_array_index = findPeerArrayOpening()) != -1)
 			{
-				perror( "Error: socket failed" );
-				exit( 1 );
+				/* Accept the client, and store it's socket ID in the clients array. */
+				if ((peers[peer_array_index].m_peer_socket = accept(seed_sock, (struct sockaddr*)&client_addr, &length)) == -1)
+				{
+					//perror("Server Error: Accepting issue"); 
+					//exit(1);
+				}
+				else
+				{
+					printf("Peer has connected.\n");
+					/* Spin off a thread to process the peer. The peer's index in the clients array is passed as a 
+					 * parameter to indicate which peer is being processed. */
+					if (pthread_create(&(peers[peer_array_index].m_thread), NULL, &client_handler, &(peers[peer_array_index].m_index)) != 0)
+					{
+						printf("Error Creating Thread\n");
+						exit(1);
+					}
+				}
 			}
+			cur = clock() - start;
+			if ((((float)cur/CLOCKS_PER_SEC) >= 10) && count_to_4 < 4)// if it's been 10 seconds
+			{
+				int increment;
+				if (cur_chunk == 0)
+				{
+					increment = 5;
+				}
+				else
+				{
+					increment = 4;
+				}
+				
+				printf("I am client_%d, and I am advertising the following chunk of the file: %d%% to %d%%.\n", client_i, cur_chunk, cur_chunk + increment);
+				
+				if(cur_chunk == 0)
+				{
+					cur_chunk++;
+				}
+				cur_chunk += 5;
+				count_to_4++;
+				
+				start = clock();
+			}
+		}
+	
+		if (close(seed_sock) != 0)
+		{
+			perror("Closing socket issue");
+		}
+	}
+	
+	char buf[CHUNK_SIZE];
+	int sent;
+	if (mode == DOWNLOAD)
+	{
+		while (1)
+		{
+			fgets(buf, sizeof(buf), stdin);
+			
+			if (strncmp(buf, "<REQ LIST>", strlen("<REQ LIST>")) == 0)
+			{
+				/* create a socket */
+				/* internet stream socket, TCP */
+				if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+				{
+					perror( "Error: socket failed" );
+					exit( 1 );
+				}
 
-			/*connect to the server */
-			/* now when we need to communicate to the server, we do it over "server_sock" */
-			if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
-			{
-				perror( "Error: Connection Issue" );
-				exit(1);
-			}
-		
-			write(server_sock, "<REQ LIST>", strlen("<REQ LIST>"));
-			memset(buf, '\0', sizeof(buf));
-			while((sent = read(server_sock, buf, sizeof(buf))) > 0)
-			{
-				printf("%s", buf);
-				memset(buf, '\0', sizeof(buf));
-			}
-		}
-		else if (strncmp(buf, "<createtracker", strlen("<createtracker")) == 0)
-		{		
-			if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
-			{
-				perror( "Error: socket failed" );
-				exit( 1 );
-			}
-			if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
-			{
-				perror( "Error: Connection Issue" );
-				exit(1);
-			}
+				/*connect to the server */
+				/* now when we need to communicate to the server, we do it over "server_sock" */
+				if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
+				{
+					perror( "Error: Connection Issue" );
+					exit(1);
+				}
 			
-			while((sent = read(server_sock, buf, sizeof(buf))) > 0)
-			{
-				printf("%s", buf);
-			}
-		}
-		else if (strncmp(buf, "<updatetracker", strlen("<updatetracker")) == 0)
-		{
-			if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
-			{
-				perror( "Error: socket failed" );
-				exit( 1 );
-			}
-			if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
-			{
-				perror( "Error: Connection Issue" );
-				exit(1);
-			}
-		
-			write(server_sock, buf, strlen(buf));
-			memset(buf, '\0', sizeof(buf));
-			while((sent = read(server_sock, buf, sizeof(buf))) > 0)
-			{
-				printf("%s", buf);
-			}
-			printf("\n");
-		}
-		else if (strncmp(buf, "<GET", strlen("<GET")) == 0)
-		{
-			FILE *file;
-			char tokenize[CHUNK_SIZE], filename[CHUNK_SIZE];
-			char *line;
-			if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
-			{
-				perror( "Error: socket failed" );
-				exit( 1 );
-			}
-			if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
-			{
-				perror( "Error: Connection Issue" );
-				exit(1);
-			}
-		
-			write(server_sock, buf, strlen(buf));
-			
-			strcpy(tokenize, buf);
-			/*Get the tracker filename*/
-			line = strtok(tokenize, " ");
-			line  = strtok(NULL, ">");
-			strcpy(filename, line);
-			
-			if (access(filename, F_OK) != 0)
-			{
-				file = fopen(filename, "wb");
-
+				write(server_sock, "<REQ LIST>", strlen("<REQ LIST>"));
 				memset(buf, '\0', sizeof(buf));
 				while((sent = read(server_sock, buf, sizeof(buf))) > 0)
 				{
-					fwrite(buf, sizeof(char), strlen(buf), file);
+					printf("%s", buf);
 					memset(buf, '\0', sizeof(buf));
 				}
-				
-				fclose(file);
-				
-				//spin off download thread.
 			}
-			else
+			else if (strncmp(buf, "<createtracker", strlen("<createtracker")) == 0)
+			{		
+				if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+				{
+					perror( "Error: socket failed" );
+					exit( 1 );
+				}
+				if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
+				{
+					perror( "Error: Connection Issue" );
+					exit(1);
+				}
+				
+				while((sent = read(server_sock, buf, sizeof(buf))) > 0)
+				{
+					printf("%s", buf);
+				}
+			}
+			else if (strncmp(buf, "<updatetracker", strlen("<updatetracker")) == 0)
 			{
-				printf("You already have this tracker file!\n");
+				if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+				{
+					perror( "Error: socket failed" );
+					exit( 1 );
+				}
+				if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
+				{
+					perror( "Error: Connection Issue" );
+					exit(1);
+				}
+			
+				write(server_sock, buf, strlen(buf));
+				memset(buf, '\0', sizeof(buf));
+				while((sent = read(server_sock, buf, sizeof(buf))) > 0)
+				{
+					printf("%s", buf);
+				}
+				printf("\n");
+			}
+			else if (strncmp(buf, "<GET", strlen("<GET")) == 0)
+			{
+				FILE *file;
+				char tokenize[CHUNK_SIZE], filename[CHUNK_SIZE];
+				char *line;
+				if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+				{
+					perror( "Error: socket failed" );
+					exit( 1 );
+				}
+				if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
+				{
+					perror( "Error: Connection Issue" );
+					exit(1);
+				}
+			
+				write(server_sock, buf, strlen(buf));
+				
+				strcpy(tokenize, buf);
+				/*Get the tracker filename*/
+				line = strtok(tokenize, " ");
+				line  = strtok(NULL, ">");
+				strcpy(filename, line);
+				
+				if (access(filename, F_OK) != 0)
+				{
+					file = fopen(filename, "wb");
+
+					memset(buf, '\0', sizeof(buf));
+					while((sent = read(server_sock, buf, sizeof(buf))) > 0)
+					{
+						fwrite(buf, sizeof(char), strlen(buf), file);
+						memset(buf, '\0', sizeof(buf));
+					}
+					
+					fclose(file);
+					
+					//spin off 5 downloads thread.
+					int i;
+					for (i = 0; i < 5; i++)
+					{
+						if (pthread_create(&(peers[i].m_thread), NULL, &download, &(peers[i].m_index)) != 0)
+						{
+							perror("Error creating download thread");
+						}
+					}
+				}
+				else
+				{
+					printf("You already have this tracker file!\n");
+				}
 			}
 		}
 	}
@@ -206,102 +321,72 @@ int main(int argc, const char* argv[])
 	return (0);
 }
 
+void *download(void * index)
+{
+	//index in array indicates what sub-chunk they want?
+	// i = 0 -> 1st sub-chunk.
+	// i = 1 -> second sub-chunk
+	int client_index = *((int *) index);
+	
+	printf("%d\n", client_index);
+	
+	//mutex lock
+	//get first part of your sub-chunk
+	//get second part
+	//get third part
+	//get fourth part
+	//get fifth part
+	//mutex unlock
+	
+	return;
+}
+
 void *client_handler(void * index)
 {
+	struct sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) };
+	struct hostent *hp;
+
+	char buf[CHUNK_SIZE];
 	/* Dereference the index passed as a parameter by the pthread_create() function */
 	int client_index = *((int *) index);
 	
+	//create tracker
+	if( ( server_sock = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
+	{
+		perror( "Error: socket failed" );
+		exit( 1 );
+	}
+	/*connect to the server */
+	/* now when we need to communicate to the server, we do it over "server_sock" */
+	if (connect( server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
+	{
+		perror( "Error: Connection Issue" );
+		exit(1);
+	}
+	
+	//create tracker
+	//get md5
+	memset(buf, '\0', sizeof(buf));
+	char *md5 = computeMD5("picture-wallpaper.jpg");
+	sprintf(buf, "<createtracker picture 35738 img %s localhost %d", md5, port); 
+	write(server_sock, buf , strlen(buf));
+	free(md5);
+	
+	
 	/******************************
 	 ******************************
-	 *             SHARE FILE HERE           *
-	 *   PEER SHOULD SND MESSAGE    *
-	 *   SAYING WHAT FILE IT WANTS   *  //Is this how we should do this?
+	 *      SHARE FILE HERE       *
+	 *   PEER SHOULD SND MESSAGE  *
+	 *  SAYING WHAT FILE IT WANTS *   //Is this how we should do this?
 	 ******************************
 	 ******************************/
 	 
-	 //UPDATE TRACKER EVERY 10 SECONDS
-	 
-	
 	/*When we're done sharing*/
 	if (close(peers[client_index].m_peer_socket) != 0)
 	{
 		perror("Closing socket issue");
 	}
 	if (pthread_cancel(peers[client_index].m_thread) != 0)
-	{
-		perror("Ending thread issue");
-		exit(1);
-	}
-	
-	return;
-}
-
-void *seed(void * param)
-{
-	/* We should pass a parameter in when executeing ./client.out to signify which port we'll be using */
-	struct sockaddr_in server_addr = {AF_INET, htons( SERVER_PORT +1 )}; //Here
-	struct sockaddr_in client_addr = {AF_INET};
-	int length = sizeof(client_addr);
-	
-	if ((seed_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		perror("Server Error: Socket Failed");
-		exit(1);
-	}
-	
-	/* Variable needed for setsokopt call */
-	int setsock = 1;
-	if(setsockopt(seed_sock, SOL_SOCKET, SO_REUSEADDR, &setsock, sizeof(setsock)) == -1)
-	{
-		perror("Server Error: Setsockopt failed");
-		exit(1);
-	}
-	
-	if (bind(seed_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1 )
-	{
-		perror("Server Error: Bind Failed");
-		exit(1);
-	}
-	
-	if (listen(seed_sock, MAX_CLIENT) == -1)
-	{
-		perror("Server Error: Listen failed");
-		exit(1);
-	}
-	
-	setUpPeerArray();
-	
-	while (1)
-	{
-		int peer_array_index;
-		/* The server searches for an opening in the client array. If the array is full, -1 is returned */
-		if ((peer_array_index = findPeerArrayOpening()) != -1)
-		{
-			/* Accept the client, and store it's socket ID in the clients array. */
-			if ((peers[peer_array_index].m_peer_socket = accept(seed_sock, (struct sockaddr*)&client_addr, &length)) == -1)
-			{
-				perror("Server Error: Accepting issue"); 
-				exit(1);
-			}
-			else
-			{
-				printf("Peer has connected.\n");
-				/* Spin off a thread to process the peer. The peer's index in the clients array is passed as a 
-				 * parameter to indicate which peer is being processed. */
-				if (pthread_create(&(peers[peer_array_index].m_thread), NULL, &client_handler, &(peers[peer_array_index].m_index)) != 0)
-				{
-					printf("Error Creating Thread\n");
-					exit(1);
-				}
-			}
-		}
-	}
-	
-	if (close(seed_sock) != 0)
-	{
-		perror("Closing socket issue");
-	}
-	if (pthread_cancel(main_seed_thread) != 0)
 	{
 		perror("Ending thread issue");
 		exit(1);
@@ -338,7 +423,7 @@ int findPeerArrayOpening()
 	return -1;
 }
 
-
+/*
 int findCommand()
 {
     int i;
@@ -381,3 +466,4 @@ void findIP()
         }
     }
 }
+*/
